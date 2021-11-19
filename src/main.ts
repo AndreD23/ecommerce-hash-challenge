90014babb0d448d62a31d0f04a6ea9e54708e5fb
André Dorneles <andre@imperiosoft.com.br>
2021-11-19 01:00:34 -0300

    Configurações de gRPC no Nest

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,18 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { grpcClientOptions } from './grpc-client.option';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
+    new FastifyAdapter(),
   );
+
+  app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+
+  await app.startAllMicroservices();
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
