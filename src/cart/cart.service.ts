0710009f32bfdea1779d1dad336a58694aa044d0
André Dorneles <andre@imperiosoft.com.br>
2021-11-25 11:42:23 -0300

    Busca de produtos no arquivo json

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -3,6 +3,9 @@ import { CheckoutCartDto } from './dto/checkout-cart.dto';
 import { Observable } from 'rxjs';
 import { Discount } from '../interfaces/discount.interface';
 import { ClientGrpc } from '@nestjs/microservices';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Product } from './entities/product.entity';
 
 interface DiscountService {
   getDiscount(productId: number): Observable<any>;
@@ -17,14 +20,36 @@ export class CartService implements OnModuleInit {
     this.discountService = this.client.getService<DiscountService>('Discount');
   }
 
+  Products = JSON.parse(
+    fs.readFileSync(path.resolve(__dirname, 'products.json'), 'utf-8'),
+  );
+
   checkout(checkoutCartDto: CheckoutCartDto) {
-    const productId = 1;
+    // Extrai as informações do BD
+    const productsCheckout = checkoutCartDto.products.map((productCart) => {
+      return this.getProductsDetails(productCart);
+    });
 
-    this.getGrpcDiscount(productId);
-    return 'This action makes a cart checkout';
+    // Verifica se há estoque suficiente
+
+    // Verificar se o serviço de desconto está disponível
+
+    // Verificar valor com desconto
+
+    // this.getGrpcDiscount();
+
+    // Verificar se é black friday
+    // Se for, adicionar produto brinde no carrinho
+    // Produto brinde possui flag is_gift = true
+    // Só pode haver 1 produto brinde no carrinho
   }
 
-  private getGrpcDiscount(productId: number): Observable<Discount> {
-    return this.discountService.getDiscount(productId);
+  private getProductsDetails(data) {
+    const productsBD = this.Products;
+    return productsBD.find((product) => product.id === data.id);
   }
+
+  // private getGrpcDiscount(productId: number): Observable<Discount> {
+  //   return this.discountService.getDiscount(productId);
+  // }
 }
--- src/cart/dto/checkout-cart.dto.ts
+++ src/cart/dto/checkout-cart.dto.ts
@@ -1,1 +1,17 @@
-export class CheckoutCartDto {}
+import { IsInt, IsNotEmpty, Min } from 'class-validator';
+
+export class ProductCheckoutCartDTO {
+  @IsNotEmpty()
+  @IsInt()
+  id: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  @Min(1)
+  quantity: number;
+}
+
+export class CheckoutCartDto {
+  @IsNotEmpty()
+  products: ProductCheckoutCartDTO[];
+}
